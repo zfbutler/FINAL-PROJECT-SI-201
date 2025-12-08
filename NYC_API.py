35a8e624e7999b4dc49695b1327f42efaa00d3ab
Zeke Butler <zfbutler@umich.edu>
2025-12-08 17:11:39 -0500

    Did a days worth of work...

--- NYC_API.py
+++ NYC_API.py
@@ -1,3 +1,4 @@
+import sqlite3
 import requests
 import unittest
 import os 
@@ -5,10 +6,54 @@ import json
 from requests.auth import HTTPBasicAuth
 
 
+
+
+
+
+
+
+
+
+
+
+
 API_KEY = "2o031k1p3cq0m8kauzmvfjk0c"
 API_SECRET = "5nj1sx54lui1b33s5ive0odx58p7yxwhnxyb4gtq8cwuso51i6"
 
 url = "https://data.cityofnewyork.us/api/v3/views/h9gi-nx95/query.json"
 
-def fetch_nyc_crashes()
+def fetch_nyc_crashes(limit=25, where=None, select=None):
+    params = {"$limit": limit}
+    if where:
+        params["$where"] = where
+    if select:
+        params["$select"] = select
+    if order:
+        params["$order"] = order
 
+    headers = {"X-App-Token": API_KEY}
+    response = requests.get(url, params=params, headers=headers)
+    
+    response.raise_for_status()
+    
+ 
+def insert_crash_info(crash):
+    conn = sqlite3.connect("weather_crashes.db")
+    cur = conn.cursor()
+    
+    cur.execute(""" INSERT OR IGNORE INTO crash_info (collision_id,location,crash_date) VALUES (?, ?, ?)""",(crash['collision_id'], crash['location'], crash['crash_date']))
+    
+    conn.commit()
+    
+    crash_info_id = cur.lasrowid
+    conn.close()
+    
+    return crash_info_id
+
+def insert_crash_more(crash):
+    conn = sqlite3.connect("weather_crashes.db")
+    cur = conn.cursor()
+    cur.execute(""" INSERT OR IGNORE INTO crash_extra (crash_info_id, number_of_persons_injured, number_of_persons_killed) VALUES (?,?,?)""", (crash_info_id, crash['number_of_persons_injured'], crash['number_of_persons_killed']))
+    
+    conn.commit()
+    conn.close()
