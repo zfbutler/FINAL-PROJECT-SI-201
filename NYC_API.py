b95c2234ea9e91ebf1b3f0b669bbfeab8d4a44be
Zeke Butler <zfbutler@umich.edu>
2025-12-07 21:05:19 -0500

    Added nyc document

---
+++ NYC_API.py
@@ -0,0 +1,14 @@
+import requests
+import unittest
+import os 
+import json
+from requests.auth import HTTPBasicAuth
+
+
+API_KEY = "2o031k1p3cq0m8kauzmvfjk0c"
+API_SECRET = "5nj1sx54lui1b33s5ive0odx58p7yxwhnxyb4gtq8cwuso51i6"
+
+url = "https://data.cityofnewyork.us/api/v3/views/h9gi-nx95/query.json"
+
+def fetch_nyc_crashes()
+
